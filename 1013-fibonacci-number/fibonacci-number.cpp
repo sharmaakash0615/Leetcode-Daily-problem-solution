89dc4e8b121f8136580eed22080d6b545e30c07c
Akash Sharma <sharmaakash0615@gmail.com>
2026-09-23 19:07:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (88.24%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,7 +1,56 @@
+// using recursion
+// class Solution {
+// public:
+//     int fib(int n) {
+//     if(n<=1) return n;
+//      return fib(n-1)+fib(n-2);
+//     }
+// };
+
+// using memoization
+
+// class Solution {
+// public:
+//     int fib(int n) {
+//     vector<int>dp(n+1,-1);
+//     if(n<=1) return n; 
+//     if(dp[n]!=-1) return dp[n];
+
+//     return dp[n]=fib(n-1)+fib(n-2);
+     
+//     }
+// };
+
+// using tabulation
+
+// class Solution {
+// public:
+//     int fib(int n) {
+//     vector<int>dp(n+1,-1);
+//     dp[0]=0;
+//     dp[1]=1;
+//     for(int i=2;i<=n;i++)
+//     {
+//       dp[i]=dp[i-1]+dp[i-2];
+//     }
+//    return dp[n];
+// }
+// };
+
+// using optimization
+
 class Solution {
 public:
     int fib(int n) {
-    if(n<=1) return n;
-     return fib(n-1)+fib(n-2);
+        if(n==0) return 0;
+    int prev2=0;
+    int prev=1;
+    for(int i=2;i<=n;i++)
+    {
+     int curr=prev+prev2;
+      prev2=prev;
+      prev=curr;
     }
+   return prev;
+}
 };
