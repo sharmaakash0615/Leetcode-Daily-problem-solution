6bcc2d0a29a23e548ff8f2b19b1e5fcbdd9553e8
Akash Sharma <sharmaakash0615@gmail.com>
2026-09-23 08:58:38 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (93.40%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -1,32 +1,83 @@
+// class Solution {
+//     private:
+//     bool bfsMulti(int start,vector<int>& vis,vector<vector<int>>& graph)
+//     {
+//      queue<int>q;
+//      q.push(start);
+//      vis[start]=0;
+//      while(!q.empty())
+//      {
+//         int node=q.front();
+//         q.pop();
+       
+//         for(auto it:graph[node])
+//         {
+           
+//             if(vis[it]==-1)
+//             {
+//             vis[it]=!vis[node];
+//             q.push(it);
+            
+//             }
+//             else if(vis[it]==vis[node])
+//             {
+//                 return false;
+//             }
+           
+//         }
+//      }
+//      return true;
+//     }
+// public:
+//     bool isBipartite(vector<vector<int>>& graph) {
+//     int n=graph.size();
+//     int m=graph[0].size();
+//     vector<int>vis(n,-1);
+    
+//     for(int i=0;i<n;i++)
+//     {
+//         if(vis[i]==-1)
+//         {
+//             if(bfsMulti(i,vis,graph)==false)
+//             return false;
+
+//         }
+       
+//     }
+    // return true;
+//     }
+// };
+
+
 class Solution {
     private:
-    bool bfsMulti(int start,vector<int>& vis,vector<vector<int>>& graph)
+    bool dfsMulti(int start,vector<int>& vis,vector<vector<int>>& graph,int prev)
     {
-     queue<int>q;
-     q.push(start);
-     vis[start]=0;
-     while(!q.empty())
-     {
-        int node=q.front();
-        q.pop();
-       
-        for(auto it:graph[node])
+        if(start==0)
         {
-           
-            if(vis[it]==-1)
-            {
-            vis[it]=!vis[node];
-            q.push(it);
-            
-            }
-            else if(vis[it]==vis[node])
-            {
-                return false;
-            }
-           
+          vis[start]=prev;
+          prev=vis[start];
         }
-     }
-     return true;
+        else{
+            vis[start]=!prev;
+            prev=vis[start];
+        }
+     
+    for(auto it:graph[start])
+    {
+        if(vis[it]==-1)
+        {
+        if(dfsMulti(it,vis,graph,prev)==false)
+          return false;
+        }
+        else if(vis[it]==vis[start])
+        {
+           return false;
+        }
+        
+     
+    }
+    return true;
     }
 public:
     bool isBipartite(vector<vector<int>>& graph) {
@@ -38,8 +89,9 @@ public:
     {
         if(vis[i]==-1)
         {
-            if(bfsMulti(i,vis,graph)==false)
+            if(dfsMulti(i,vis,graph,0)==false)
             return false;
+            
         }
        
     }
